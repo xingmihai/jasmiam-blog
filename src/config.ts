8933b754d23176368ed5dc6fb29e28fefbddba53
JASMIAM <69405366+1498934815@users.noreply.github.com>
2025-09-10 13:59:36 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,18 @@
 export default {
   // 网站标题
-  Title: '韩小韩博客',
+  Title: '静谧幻想家博客',
   // 网站地址
-  Site: 'https://www.vvhan.com',
+  Site: 'jasmiam.dpdns.org',
   // 网站副标题
   Subtitle: '不曾与你分享的时间,我在进步.',
   // 网站描述
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description: '静谧幻想家博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
   // 网站作者
   Author: '.𝙃𝙖𝙣',
   // 作者头像
-  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=640',
+  Avatar: 'https://q1.qlogo.cn/g?b=qq&nk=1498934815&s=640',
   // 网站座右铭
-  Motto: '运气是计划之外的东西.',
+  Motto: '除了死亡，一切失败都只存在于心理层面.',
   // Cover 网站缩略图
   Cover: '/assets/images/banner/072c12ec85d2d3b5.webp',
   // 网站侧边栏公告 (不填写即不开启)
