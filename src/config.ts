6007a66e83f70c49cb8747449ee25b441100de8f
JASMIAM <69405366+1498934815@users.noreply.github.com>
2025-09-11 02:16:06 +0800

    更新 config.ts

--- src/config.ts
+++ src/config.ts
@@ -62,8 +62,6 @@ export default {
     // 仅支持 SVG 且 SVG 需放在 public/assets/images/svg/ 目录下，填入文件名即可 <不需要文件后缀名>（封装了 SVG 组件 为了极致压缩 SVG）
     // 建议使用 https://tabler.io/icons 直接下载 SVG
     { text: 'Github', link: 'https://github.com/1498934815', icon: 'WebSite_github' },
-    { text: '每日热榜', link: 'https://hot.vvhan.com', icon: 'WebSite_hot' },
-    { text: '骤雨重山图床', link: 'https://wp-cdn.4ce.cn', icon: 'WebSite_img' },
     { text: 'Analytics', link: 'https://analytics.jasmiam.top', icon: 'WebSite_analytics' },
   ],
   // 侧边栏展示
