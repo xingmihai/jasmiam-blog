ea19efea31fcd628e62ee1b9b9189009d5e7d674
JASMIAM <69405366+1498934815@users.noreply.github.com>
2025-09-11 21:28:39 +0800

    Update config.ts via web editor

--- src/config.ts
+++ src/config.ts
@@ -65,9 +65,6 @@ export default {
     { text: '骤雨重山图床', link: 'https://img.jasmiam.top', icon: 'WebSite_img' },
     { text: 'Analytics', link: 'https://analytics.jasmiam.top', icon: 'WebSite_analytics' },
     { text: 'MT论坛', link: 'https://bbs.binmt.cc/home.php?mod=space&uid=147108&do=profile', icon: 'WebSite_mt' },
-    { text: '微信', link: 'https://img.jasmiam.top/v2/ROIwNPT.png', icon: 'WebSite_wechat' },
-    { text: 'bilibili', link: 'https://b23.tv/EscDwn5', icon: 'WebSite_bilibili' },
-    { text: 'QQ群', link: 'mqqapi://card/show_pslcard?src_type=internal&version=1&uin=1054810204&card_type=group&source=', icon: 'WebSite_group' },
   ],
   // 侧边栏展示
   AsideShow: {
