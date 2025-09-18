bac6163e5b6f62344fee8c885748f4ca989661de
JASM1AM <1498934815@qq.com>
2025-09-18 08:01:04 +0800

    更新 astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,11 +17,11 @@ import swup from '@swup/astro';
 // https://astro.build/config
 export default defineConfig({
 	site: SITE_INFO.Site,
-	build: { assets: 'ji_static' },
+	build: { assets: 'vh_static' },
 	integrations: [swup({
 		theme: false,
-		animationClass: "vh-animation-",
-		containers: [".main-inner>.main-inner-content",'.vh-header>.main'],
+		animationClass: "ji-animation-",
+		containers: [".main-inner>.main-inner-content",'.ji-header>.main'],
 		smoothScrolling: true,
 		progress: true,
 		cache: true,
