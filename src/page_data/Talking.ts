703ddb801cd2d108602776eb8f4c2d22cbfd735e
JASM1AM <1498934815@qq.com>
2025-09-18 08:27:49 +0800

    更新 Talking.ts

--- src/page_data/Talking.ts
+++ src/page_data/Talking.ts
@@ -2,7 +2,7 @@ export default {
   // API 接口请求优先，数据格式保持和 data 一致
   api: 'https://blogapi.jasmiam.top/Talking.ts',
   // api 为空则使用 data 静态数据 
-  // 注意：图片请用 ji-img-flex 类包裹
+  // 注意：图片请用 vh-img-flex 类包裹
   data: [
     {
       "date": "2025-02-12 19:36:16",
@@ -10,7 +10,7 @@ export default {
         "测试",
         "测试"
       ],
-      "content": "好美🌲<p class=\"ji-img-flex\"><img src=\"https://q.qlogo.cn/headimg_dl?dst_uin=1498934815&spec=640\"></p>"
+      "content": "好美🌲<p class=\"vh-img-flex\"><img src=\"https://q.qlogo.cn/headimg_dl?dst_uin=1498934815&spec=640\"></p>"
     },
     {
       "date": "2024-10-08 18:18:18",
