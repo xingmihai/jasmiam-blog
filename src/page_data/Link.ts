861fafa3a6e376b6ebad457009cc322dfd477727
JASM1AM <1498934815@qq.com>
2025-09-20 23:28:02 +0800

    更新 Link.ts

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -8,6 +8,24 @@ export default {
       "link": "https://www.neacg.cn",
       "avatar": "https://q1.qlogo.cn/g?b=qq&nk=2787939642&s=640",
       "descr": "联系QQ：2787939642"
-    }
+    },
+    {
+      "name": "易搜特收录网",
+      "link": "https://www.esoot.com",
+      "avatar": "www.esoot.com/zb_users/theme//JackSite/public/favicon.ico",
+      "descr": ""
+    },
+    {
+      "name": "小鹅导航",
+      "link": "https://www.xedh.cn",
+      "avatar": "www.wxhao.cn/favicon.ico",
+      "descr": ""
+    },
+    {
+      "name": "自动秒收录",
+      "link": "http://www.wxhao.cn/",
+      "avatar": "www.wxhao.cn/favicon.ico",
+      "descr": ""
+    },
   ]
 }
