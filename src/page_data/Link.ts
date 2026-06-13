b5e86d73a20ada4072dd3747646d4efa8963fad2
星觅海 <1498934815@qq.com>
2026-06-14 04:12:53 +0800

    更新 Link.ts

--- src/page_data/Link.ts
+++ src/page_data/Link.ts
@@ -9,5 +9,11 @@ export default {
       "avatar": "https://blog.xiaow.qzz.io/favicon.ico",
       "descr": "一起分享，共同进步"
     },
+    {
+    name: 韩小韩博客
+    link: https://www.vvhan.com/
+    avatar: https://q1.qlogo.cn/g?b=qq&nk=1655466387&s=100
+    desc: 运气是计划之外的东西.
+    }
   ]
 }
