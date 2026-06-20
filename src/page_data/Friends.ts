a04f2aa847ebece7196f513e2bf911fc28a65b9b
xingmihai <1498934815@gmail.com>
2026-06-20 08:57:26 +0800

    update Friends.ts

--- src/page_data/Friends.ts
+++ src/page_data/Friends.ts
@@ -1,6 +1,6 @@
 export default {
   // API 接口请求优先，数据格式保持和 data 一致
-  api: 'https://xingmihai.github.io/rss-aggregator/public/articles.json',
+  api: 'https://rss.xmhai.cn/articles.json',
   // api 为空则使用 data 静态数据
   data: [
     {
